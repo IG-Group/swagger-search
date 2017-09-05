16543dfbd1c8416cdf361de4e8cc6972c3ffc813
Dan Lebrero <dlebrero@gmail.com>
2017-09-06 00:25:34 +0200

    splitting the deploy in two: one build increments the version number and tags, the other builds based on the tag
    This is because Travis integration with GitHub Releases just works for tags

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,19 +2,6 @@
 
 set -eu
 
-if [[ "${TRAVIS_BRANCH}" != "master" ]]; then
-    exit 0
-fi
-
-if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
-    exit 0
-fi
-
-AUTOMATED_AUTHOR_EMAIL=builds@travis-ci.com
-LAST_COMMIT_AUTHOR_EMAIL=$(git --no-pager show -s --format='%ae' HEAD)
-echo "last commit ID: $LAST_COMMIT_AUTHOR_EMAIL"
-test $LAST_COMMIT_AUTHOR_EMAIL != $AUTOMATED_AUTHOR_EMAIL
-
 increment_version ()
 {
   declare -a part=( ${1//\./ } )
@@ -30,22 +17,39 @@ increment_version ()
   echo -e "${new// /.}"
 }
 
+
+if [[ "${TRAVIS_BRANCH}" != "master" ]]; then
+    exit 0
+fi
+
+if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
+    exit 0
+fi
+
+AUTOMATED_AUTHOR_EMAIL=builds@travis-ci.com
+LAST_COMMIT_AUTHOR_EMAIL=$(git --no-pager show -s --format='%ae' HEAD)
+echo "last commit ID: $LAST_COMMIT_AUTHOR_EMAIL"
 VERSION=`cat version`
-echo "Building version $VERSION"
-increment_version $VERSION > version
 
-# CREATE GIT TAG
-git config --global user.email $AUTOMATED_AUTHOR_EMAIL
-git config --global user.name "Travis CI"
-git checkout "$TRAVIS_BRANCH"
+if [ $LAST_COMMIT_AUTHOR_EMAIL != $AUTOMATED_AUTHOR_EMAIL ]; then
 
-lein with-profile +set-version set-version $VERSION
-lein with-profile +not-lib uberjar
-lein deploy releases
+    # CREATE GIT TAG
+    git config --global user.email $AUTOMATED_AUTHOR_EMAIL
+    git config --global user.name "Travis CI"
+    git checkout "$TRAVIS_BRANCH"
 
-export GIT_TAG=v$VERSION
-git commit -m "Set next build VERSION number" version
-git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
-echo "Tag done"
-git push --tags --quiet https://$GITHUBKEY@github.com/IG-Group/swagger-search master > /dev/null 2>&1
-echo "Pushing"
+    increment_version $VERSION > version
+    echo "Current version $VERSION, next version `cat version`"
+    export GIT_TAG=v$VERSION
+    git commit -m "Set release build VERSION number" version
+    git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
+    echo "Tag done"
+    git push --tags --quiet https://$GITHUBKEY@github.com/IG-Group/swagger-search master > /dev/null 2>&1
+    echo "Pushing"
+
+else
+    echo "Deploying version $VERSION"
+    lein with-profile +set-version set-version $VERSION
+    lein with-profile +not-lib uberjar
+    lein deploy releases
+fi
