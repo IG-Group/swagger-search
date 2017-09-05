58458bc214e356e18d23c976033cc245d6849670
Dan Lebrero <dlebrero@gmail.com>
2017-09-05 09:28:52 +0200

    using current date as version number

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -26,21 +26,21 @@ increment_version ()
   echo -e "${new// /.}"
 }
 
-VERSION=`cat version`
+VERSION="`date +%Y%m%d`.$TRAVIS_BUILD_NUMBER"
 echo "Building version $VERSION"
-increment_version $VERSION > version
 
 # CREATE GIT TAG
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
-git checkout "$TRAVIS_BRANCH"
+#git checkout "$TRAVIS_BRANCH"
 export GIT_TAG=v$VERSION
-git commit -m "Set next build VERSION number" version
+#git commit -m "Set next build VERSION number" version
 git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
 echo "Tag done"
-git push --tags --quiet https://$GITHUBKEY@github.com/IG-Group/swagger-search master > /dev/null 2>&1
+git push --quiet https://$GITHUBKEY@github.com/IG-Group/swagger-search $GIT_TAG > /dev/null 2>&1
 echo "Pushing"
 
 
 lein with-profile +set-version set-version $VERSION
 lein with-profile +not-lib uberjar
+ls -l target
